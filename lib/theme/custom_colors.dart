6d8c52a7db01e0084f7aa029be400bcba7bc0b3b
nikitapits <pitsnikita@gmail.com>
2023-05-24 16:07:31 +0300

    created project

---
+++ lib/theme/custom_colors.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+class CustomColors {
+  static const Color blue = Color(0xFF1A6EEB);
+  static const Color lightBlue = Color(0xFF07BBF9);
+  static const Color purple = Color(0xFF8B5DEE);
+  static const Color red = Color(0xFFEB3838);
+  static const Color orange = Color(0xFFEF6036);
+  static const Color pink = Color(0xFFFAA9CB);
+  static const Color green3 = Color(0xFF33C25B);
+  static const Color green2 = Color(0xFF62ECAA);
+  static const Color yellow2 = Color(0xFFFFCC3E);
+  static const Color yellow = Color(0xFFF4FB1A);
+  static const Color blue40 = Color(0x401A6EEB);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color bg = Color(0xFFF5F5F5);
+  static const Color bg060 = Color(0x60F5F5F5);
+  static const Color gray = Color(0xFFE0E0E0);
+  static const Color gray1 = Color(0xFFD8D8D8);
+  static const Color gray2 = Color(0xFFAFAFAF);
+  static const Color gray3 = Color(0xFF909090);
+  static const Color gray4 = Color(0xFF7D7D7F);
+  static const Color gray5 = Color(0xFF6A696C);
+  static const Color gray6 = Color(0xFF454448);
+  static const Color black = Color(0xFF17151A);
+  static const Color black60 = Color(0x6017151A);
+  static const Color blueBg = Color(0xFFD1E0F1);
+  static const Color blueIcon = Color(0xFFA6BED9);
+}
